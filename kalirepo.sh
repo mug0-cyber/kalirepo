14738c1d3f65043fb8d0945be4784536e0381689
mug0-cyber <59515185+mug0-cyber@users.noreply.github.com>
2021-09-20 18:29:09 +0300

    Update kalirepo.sh

--- kalirepo.sh
+++ kalirepo.sh
@@ -1,4 +1,5 @@
 wget -q -O - https://archive.kali.org/archive-key.asc  | apt-key add
 apt update
-apt-get install scala -y
-sudo dpkg -P scala
+apt upgrade -y
+sudo dpkg -i --force-overwrite /var/cache/apt/archives/util*
+apt upgrade -y
