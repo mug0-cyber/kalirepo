956080c0e306ffa3522b67090a3219a51f27891e
mug0-cyber <59515185+mug0-cyber@users.noreply.github.com>
2021-09-20 18:57:03 +0300

    Update kalirepo.sh

--- kalirepo.sh
+++ kalirepo.sh
@@ -6,4 +6,5 @@ apt upgrade -y
 sudo dpkg -i --force-overwrite /var/cache/apt/archives/kali-*
 apt upgrade -y
 apt-get install python3-pip -y && pip3 install setuptools
-sudo apt install libcurl4-openssl-dev libssl-dev
+sudo apt install libcurl4-openssl-dev libssl-dev -y
+sudo apt-get install python3-requests -y
